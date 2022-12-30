4295ec32ba476603889ff74e24d970b181bba755
yousamaa <usamayousaf177@gmail.com>
2022-12-30 12:30:31 +0500

    RESOLVED ISSUE OF CLASS NOT FOUND IN STI AND SANITIZED USER ATTRIBUTES FOR SIGNUP AND ACCONT UPDATE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name type phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name type phone_number])
+  end
 end
