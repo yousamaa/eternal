a350c8fa57783694b0ed4574967e47f4a2063c4f
yousamaa <usamayousaf177@gmail.com>
2022-12-31 12:34:09 +0500

    RUBOCOP AUTOCORRECTION

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Developer < User
-  has_many :games
+  has_many :games, dependent: :destroy
 end
--- app/models/game_user.rb
+++ app/models/game_user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GameUser < ApplicationRecord
   belongs_to :game
   belongs_to :gamer
