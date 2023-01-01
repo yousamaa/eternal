ab7d918a18c4d8c0b4c2bf7438ee36b9fb57e5ac
yousamaa <usamayousaf177@gmail.com>
2023-01-01 21:57:06 +0500

    CART CONTROLLER DONE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,20 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :current_cart
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name type phone_number])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name type phone_number])
+  end
+
+  def current_cart
+    @current_cart ||= Cart.find_by(id: session[:cart_id])
+    return @current_cart unless @current_cart.nil?
+
+    @current_cart = Cart.create
+    session[:cart_id] = @current_cart.id
   end
 end
