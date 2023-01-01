acb49f446f2a1345060ad49bb21c1bb4baad98c4
Hasnat Raza <hasnatraza202@gmail.com>
2023-01-01 07:32:58 +0500

    Added Ability Model CanCan

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'home#index'
+  
   devise_for :users
+  devise_for :gamers, only: :registrations
+  devise_for :developers, only: :registrations
+
 
   resources :gamers, :admins, :carts, :orders
 
